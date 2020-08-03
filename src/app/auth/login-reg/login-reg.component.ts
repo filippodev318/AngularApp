fa3587a5a5058ccad6b6a7b6cf0ce1c3378ee9e5
filippodev318 <fili.perna@gmail.com>
2020-08-03 17:52:01 +0200

    Controlli su pagina registrazione

--- src/app/auth/login-reg/login-reg.component.ts
+++ src/app/auth/login-reg/login-reg.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import {HttpClient} from '@angular/common/http'
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-reg',
@@ -10,37 +10,59 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class LoginRegComponent implements OnInit {
 
+  form! : FormGroup;
 
   email:String;
-  rusername:String;
   password:String;
-  remail:String;
-  rpassword:String;
-  rnome:String;
-  rcognome:String;
-  rcitta:String;
-  rtelefono:String;
+  regusername = new FormControl("", [
+    Validators.required,
+    Validators.minLength(5)
+  ]);
+  regemail = new FormControl("", [
+    Validators.required
+  ]);
+  regpassword = new FormControl("", [
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+  regnome = new FormControl("", [
+    Validators.required
+  ]);
+  regcognome = new FormControl("", [
+    Validators.required
+  ]);
+  regcitta = new FormControl("", [
+    Validators.required
+  ]);
+  regtelefono = new FormControl("", [
+    Validators.required,
+    Validators.pattern("[0-9 ]{10}")
+  ]);
 
   constructor(private snackBar:MatSnackBar, private http:HttpClient){
 
   }
+
+  ngOnInit(): void {
+  }
+
   register() {
 
-    let url = "http://127.0.0.1:5000/register"
+    //let url = "http://127.0.0.1:5000/register"
+    let url = "http://httpbin.org/post"
     this.http.post(url,{
-      email:this.remail,
-      username:this.rusername,
-      password:this.rpassword,
-      nome:this.rnome,
-      cognome:this.rcognome,
-      citta:this.rcitta,
-      telefono:this.rtelefono
+      email:this.regemail,
+      username:this.regusername,
+      password:this.regpassword,
+      nome:this.regnome,
+      cognome:this.regcognome,
+      citta:this.regcitta,
+      telefono:this.regtelefono
 
     }).toPromise().then((data: any) => {
       console.log(data)
       console.log(JSON.stringify(data))
     })
-
   }
   login() {
     if(this.email=="admin" && this.password=="admin"){
@@ -48,9 +70,6 @@ export class LoginRegComponent implements OnInit {
     }else{
       this.snackBar.open('Login error','',{duration:1000})
     }
-  }
-
-  ngOnInit(): void {
   }
 
 }
