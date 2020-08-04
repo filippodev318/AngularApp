9ca2a468f2483e8031a8543769ff84abb8700c90
filippodev318 <fili.perna@gmail.com>
2020-08-04 10:28:48 +0200

    Fix Pagina Registrazione

--- src/app/auth/login-reg/login-reg.component.ts
+++ src/app/auth/login-reg/login-reg.component.ts
@@ -10,34 +10,35 @@ import { FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
 })
 export class LoginRegComponent implements OnInit {
 
-  form! : FormGroup;
+  regForm = new FormGroup({
+    regusername:new FormControl("", [
+      Validators.required,
+      Validators.minLength(5)
+    ]),
+    regemail:new FormControl("", [
+      Validators.required
+    ]),
+    regpassword:new FormControl("", [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+    regnome:new FormControl("", [
+      Validators.required
+    ]),
+    regcognome:new FormControl("", [
+      Validators.required
+    ]),
+    regcitta:new FormControl("", [
+      Validators.required
+    ]),
+    regtelefono:new FormControl("", [
+      Validators.required,
+      Validators.pattern("[0-9 ]{10}")
+    ])
+  })
 
   email:String;
-  password:String;
-  regusername = new FormControl("", [
-    Validators.required,
-    Validators.minLength(5)
-  ]);
-  regemail = new FormControl("", [
-    Validators.required
-  ]);
-  regpassword = new FormControl("", [
-    Validators.required,
-    Validators.minLength(6)
-  ]);
-  regnome = new FormControl("", [
-    Validators.required
-  ]);
-  regcognome = new FormControl("", [
-    Validators.required
-  ]);
-  regcitta = new FormControl("", [
-    Validators.required
-  ]);
-  regtelefono = new FormControl("", [
-    Validators.required,
-    Validators.pattern("[0-9 ]{10}")
-  ]);
+  password:String; 
 
   constructor(private snackBar:MatSnackBar, private http:HttpClient){
 
@@ -46,23 +47,25 @@ export class LoginRegComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  register() {
+  getErrorMessage() {
+    if (this.regForm.get('regemail').hasError('required')) 
+      return 'Il campo email va compilato';
+    }
+
+  register(regForm) {
 
     //let url = "http://127.0.0.1:5000/register"
     let url = "http://httpbin.org/post"
-    this.http.post(url,{
-      email:this.regemail,
-      username:this.regusername,
-      password:this.regpassword,
-      nome:this.regnome,
-      cognome:this.regcognome,
-      citta:this.regcitta,
-      telefono:this.regtelefono
-
-    }).toPromise().then((data: any) => {
+    if(regForm.valid){
+    this.http.post(url,regForm.value).toPromise().then((data: any) => {
       console.log(data)
       console.log(JSON.stringify(data))
     })
+    this.snackBar.open('Registrazione avvenuta con successo !!','',{duration:1000})
+  }
+
+  regForm.reset()
+   
   }
   login() {
     if(this.email=="admin" && this.password=="admin"){
