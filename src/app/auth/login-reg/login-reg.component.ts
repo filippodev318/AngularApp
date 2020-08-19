f11ade5130f2cd3ebf06230ba86ccf12f1e9342c
ciromanfredi <ciromanfredi96@gmail.com>
2020-08-19 16:44:56 +0200

    ...

--- src/app/auth/login-reg/login-reg.component.ts
+++ src/app/auth/login-reg/login-reg.component.ts
@@ -11,7 +11,7 @@ import {AuthService} from '../../_service/auth-service';
   styleUrls: ['./login-reg.component.css']
 })
 export class LoginRegComponent implements OnInit {
-  backend="http://93.55.184.20:5000/"
+  backend="http://93.55.184.20:5000"
 
   logForm = new FormGroup({
     email:new FormControl("", [
